b63d78b5852f924568a15b1d51318e55b552be22
akshrajput07 <akkink.rajput@gmail.com>
2025-01-01 03:58:50 +0530

    change route names

--- pages/index.tsx
+++ pages/index.tsx
@@ -174,7 +174,7 @@ export default function Home() {
       </div>
 
       <div className="mt-8 flex justify-center">
-        <Link href="/subpages/add-menu" className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600">
+        <Link href="/add-menu" className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600">
           Add Your Menu
         </Link>
       </div>
